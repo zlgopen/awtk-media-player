a8983bb31028e8b5e3fdcc3836913622710676e5
xianjimli <xianjimli@gmail.com>
2020-03-08 09:06:31 +0800

    add config.h

---
+++ 3rd/ffmpeg/x264/x264_config.h
@@ -0,0 +1,6 @@
+#define X264_GPL           0
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     8
+#define X264_CHROMA_FORMAT X264_CSP_I422
+#define X264_VERSION ""
+#define X264_POINTVER "0.159.x"
